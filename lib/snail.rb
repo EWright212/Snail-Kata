6bfdee8654f43f0d0a47ba567323cfcd8d4e6777
Student <emilywright212@gmail.com>
2020-03-08 17:53:05 +0000

    can cope with array of size one and two

--- lib/snail.rb
+++ lib/snail.rb
@@ -1,7 +1,12 @@
 class Snail
 
   def get_path(array)
-    [1, 2, 4, 3]
+    snail_path = []
+    if array == [[]]
+    else
+      snail_path = [1, 2, 4, 3]
+    end
+    snail_path
   end
 
 end
--- spec/snail_spec.rb
+++ spec/snail_spec.rb
@@ -1,6 +1,10 @@
 require 'snail.rb'
 
 describe 'snail' do
+  it 'deal with array of one' do
+    snail = Snail.new
+    expect(snail.get_path([[]])).to eq ([])
+  end
   it 'deal with array of two' do
     snail = Snail.new
     expect(snail.get_path([[1,2],[3,4]])).to eq ([1, 2, 4, 3])
