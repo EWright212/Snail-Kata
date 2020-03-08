9b3663c6be5382e2965b830005b1787b645be112
Student <emilywright212@gmail.com>
2020-03-08 18:00:43 +0000

    Refactor for array of two

--- lib/snail.rb
+++ lib/snail.rb
@@ -4,7 +4,12 @@ class Snail
     snail_path = []
     if array == [[]]
     else
-      snail_path = [1, 2, 4, 3]
+      for i in (0...array[0].length)
+        snail_path.push(array[0][i])
+      end
+      for i in (0...array[1].length)
+        snail_path.push(array[1][1-i])
+      end
     end
     snail_path
   end
--- spec/snail_spec.rb
+++ spec/snail_spec.rb
@@ -9,4 +9,8 @@ describe 'snail' do
     snail = Snail.new
     expect(snail.get_path([[1,2],[3,4]])).to eq ([1, 2, 4, 3])
   end
+  # it 'deal with array of three' do
+  #   snail = Snail.new
+  #   expect(snail.get_path([[1,2,3],[4,5,6],[7,8,9]])).to eq ([1, 2, 3, 6, 9, 8, 7, 4, 5])
+  # end
 end
