9d5f5ab94f3871507790d29fd2cbac559f68b81e
Student <emilywright212@gmail.com>
2020-03-08 18:45:29 +0000

    Snail works for array of four items

--- lib/snail.rb
+++ lib/snail.rb
@@ -4,32 +4,55 @@ class Snail
     snail_path = []
     if array == [[]]
     else
-      # step 1 - top
+      p array
       n = array[0].length
-      for i in (0...n)
-        snail_path.push(array[0][i])
-      end
-      # step 2 - left
-      if n > 2
-        for i in (1..n-2)
-          p i
-          snail_path.push(array[i][n-1])
+      next_iteration_array = array.dup
+      for i in (0...n/2)
+        # step 1 - top
+        p "array: " + array.to_s
+        p "n: " + n.to_s
+        for i in (0...n)
+          snail_path.push(array[0][i])
         end
-      end
-      # step 3 - bottom
-      for i in (0...n)
-        snail_path.push(array[n-1][n-1-i])
-      end
-      # step 4 - left
-      if n > 2
-        for i in (1..n-2)
-          p i
-          snail_path.push(array[i][0])
+        next_iteration_array.delete_at(0)
+        p "next_iteration_array after delete1: " + next_iteration_array.to_s
+        # step 2 - left
+        if n > 2
+          for i in (1..n-2)
+            snail_path.push(array[i][n-1])
+          end
+          next_iteration_array.each_with_index do |item, idx|
+            if idx < (n-2)
+              item[n-1] = nil
+            end
+          end
         end
-      end
-      # step 5 - middle (to be changed to repeat)
-      if n == 3
-        snail_path.push(array[1][1])
+        p "next_iteration_array after delete2: " + next_iteration_array.to_s
+        # step 3 - bottom
+        for i in (0...n)
+          snail_path.push(array[n-1][n-1-i])
+        end
+        next_iteration_array.delete_at(n-2)
+        p "next_iteration_array after delete3: " + next_iteration_array.to_s
+        # step 4 - left
+        if n > 2
+          for i in (1..n-2)
+            snail_path.push(array[n-1-i][0])
+          end
+          next_iteration_array.each_with_index do |item, idx|
+            if idx < (n-2)
+              item[0] = nil
+            end
+          end
+        end
+        p "next_iteration_array after delete4: " + next_iteration_array.to_s
+        # step 5 - middle (to be changed to repeat)
+        if n == 3
+          snail_path.push(array[1][1])
+        end
+        n = n-2
+        array = next_iteration_array.dup
+        array.each &:compact!
       end
     end
     snail_path
--- spec/snail_spec.rb
+++ spec/snail_spec.rb
@@ -13,4 +13,8 @@ describe 'snail' do
     snail = Snail.new
     expect(snail.get_path([[1,2,3],[4,5,6],[7,8,9]])).to eq ([1, 2, 3, 6, 9, 8, 7, 4, 5])
   end
+  it 'deal with array of four' do
+    snail = Snail.new
+    expect(snail.get_path([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])).to eq ([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10])
+  end
 end
