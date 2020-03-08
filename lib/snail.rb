ff048feffb03c157142bb46744b80cd2797771cc
Student <emilywright212@gmail.com>
2020-03-08 18:12:29 +0000

    Snail can cope with array of three

--- lib/snail.rb
+++ lib/snail.rb
@@ -4,12 +4,32 @@ class Snail
     snail_path = []
     if array == [[]]
     else
+      # step 1 - top
       n = array[0].length
       for i in (0...n)
         snail_path.push(array[0][i])
       end
+      # step 2 - left
+      if n > 2
+        for i in (1..n-2)
+          p i
+          snail_path.push(array[i][n-1])
+        end
+      end
+      # step 3 - bottom
       for i in (0...n)
         snail_path.push(array[n-1][n-1-i])
+      end
+      # step 4 - left
+      if n > 2
+        for i in (1..n-2)
+          p i
+          snail_path.push(array[i][0])
+        end
+      end
+      # step 5 - middle (to be changed to repeat)
+      if n == 3
+        snail_path.push(array[1][1])
       end
     end
     snail_path
