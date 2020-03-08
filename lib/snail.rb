dba267089b5922496a55295556e7f70f7ed5c98f
Student <emilywright212@gmail.com>
2020-03-08 17:49:43 +0000

    Snail can cope with array of two items

---
+++ lib/snail.rb
@@ -0,0 +1,7 @@
+class Snail
+
+  def get_path(array)
+    [1, 2, 4, 3]
+  end
+
+end
--- spec/snail_spec.rb
+++ spec/snail_spec.rb
@@ -1,1 +1,8 @@
 require 'snail.rb'
+
+describe 'snail' do
+  it 'deal with array of two' do
+    snail = Snail.new
+    expect(snail.get_path([[1,2],[3,4]])).to eq ([1, 2, 4, 3])
+  end
+end
